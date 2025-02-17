1ddc6ccabc9f4a68729ad4b0c60b0139a45ed5f9
InbarDahan <inbardahan1@mail.tau.ac.il>
2025-02-17 10:44:28 +0800

    Your commit message

---
+++ r_scripts/sha_data_exploration.R
@@ -0,0 +1,13 @@
+
+
+                # Mediterranean Sea 
+                 # data exploration
+
+# packages:
+library(dplyr)
+library(ggplot2)
+
+# read data:
+data_cleaned <- read.csv("./data/raw/shahar/data_cleaned.csv")
+Readme <- read.csv("./data/raw/shahar/Readme.csv")
+
